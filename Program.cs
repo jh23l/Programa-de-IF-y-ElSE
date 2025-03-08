67234d85d40965165f46ae9c7ddc247d0a7bb9df
jh23l <2024-0023@unad.edu.do>
2025-03-08 17:51:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_de_IF_y_ElSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
